bc0d6ee93de4e8d4a8a5c0a2dd210ee9bd16be2d
Clemens Sahs <clemens.sahs@slides-worker.org>
2014-02-05 16:06:04 +0100

    change typo

--- tests/test_twitter.js
+++ tests/test_twitter.js
@@ -1,7 +1,7 @@
 casper.options.logLevel = "error";
 casper.options.verbose = false;
 
-casper.test.begin('check meetup widget', function suite(test) {
+casper.test.begin('check twitter widget', function suite(test) {
     casper
 
     .start('http://127.0.0.1:4000/index.html')
